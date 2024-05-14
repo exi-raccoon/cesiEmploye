108c73017d08351bdd218dae0591bc48a09d7413
Alexis <105306948+exi-raccoon@users.noreply.github.com>
2024-05-14 09:47:44 +0200

    added Employe class attributs, getter and setters

--- src/common/models/Employe.java
+++ src/common/models/Employe.java
@@ -2,4 +2,47 @@ package common.models;
 
 public class Employe {
 
+	private int Id;
+
+	private String Name;
+
+	private String Post;
+
+	private Double Salary;
+
+	public Employe() {
+	}
+
+	public int GetId() {
+		return Id;
+	}
+
+	public void SetId(int id) {
+		Id = id;
+	}
+
+	public String GetName() {
+		return Name;
+	}
+
+	public void SetName(String name) {
+		Name = name;
+	}
+
+	public String GetPost() {
+		return Post;
+	}
+
+	public void SetPost(String post) {
+		Post = post;
+	}
+
+	public Double GetSalary() {
+		return Salary;
+	}
+
+	public void SetSalary(Double salary) {
+		Salary = salary;
+	}
+
 }
